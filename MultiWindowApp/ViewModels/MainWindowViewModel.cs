6125e340ed4df681b174b15bb84a014d88dec66f
College <perevoznikovleonid@gmail.com>
2026-06-15 11:26:17 +0300

    Изначальный коммит

---
+++ MultiWindowApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace MultiWindowApp.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ MultiWindowApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace MultiWindowApp.ViewModels;
+
+public abstract class ViewModelBase : ReactiveObject
+{
+}
