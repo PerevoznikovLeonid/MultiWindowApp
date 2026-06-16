8a8abac711e5037fcfa930c99e453dda06e24742
College <perevoznikovleonid@gmail.com>
2026-06-16 15:31:19 +0300

    Доработка View

--- MultiWindowApp/Models/DAOs/UserDao.cs
+++ MultiWindowApp/Models/DAOs/UserDao.cs
@@ -8,10 +8,9 @@ public class UserDao
     public int Id { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
     public Gender Gender { get; set; } = Gender.Unspecified;
     public DateOnly BirthDate { get; set; }
-    public bool IsAdmin { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty; public bool IsAdmin { get; set; }
     public bool IsDeleted { get; set; }
 }
